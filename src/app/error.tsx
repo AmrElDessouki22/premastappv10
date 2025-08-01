ea6c3e414ed2ef4759910aaa632e0bce00765281
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-08-01 12:00:28 +0300

    Add src/app/error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Error() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1>Something went wrong!</h1>
+    </div>
+  );
+}
